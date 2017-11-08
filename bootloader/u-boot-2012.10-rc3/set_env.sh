f223aeefa589cd6c7f5897e9136f3eb86a48bd76
kevin <812964073@qq.com>
2017-11-08 16:14:20 +0800

    save .config

--- bootloader/u-boot-2012.10-rc3/set_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export PATH=/opt/arm/usr/bin:$PATH
-mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "set envs but address script" -d ./set_env.script ./set_env.uscr
-#cp ./set_env.uscr ../Release_version
-
